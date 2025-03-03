70538e56abd77093898d9d35b58cc2f2dabe0058
ysntopbas <ysntpbss@gmail.com>
2025-03-04 00:44:28 +0300

    feat: integrate Firebase core and update project configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:cinematev2/firebase_options.dart';
 import 'package:cinematev2/screens/login_page.dart';
 import 'package:cinematev2/screens/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
